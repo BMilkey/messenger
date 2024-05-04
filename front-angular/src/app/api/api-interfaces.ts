55e6a3719fb88fe5e0396e3f0328076f3bbef956
matelq <109665667+matelq@users.noreply.github.com>
2024-05-04 11:50:41 +0500

    front ts types from go struct

--- front-angular/src/app/api/api-interfaces.ts
+++ front-angular/src/app/api/api-interfaces.ts
@@ -1,3 +1,6 @@
+import { Time } from "@angular/common";
+import { Timestamp } from "rxjs";
+
 export interface RegisterBody {
   login: string;
   password: string;
@@ -7,4 +10,32 @@ export interface RegisterBody {
 export interface SignInBody {
   login: string;
   password: string;
+}
+
+export interface Chat {
+  id: string;
+  link: string;
+  title: string;
+  user_id: string;
+  create_time: Date;
+  about: string;
+  image_id: string;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  link: string;
+  about: string;
+  last_connection: string;
+  image_id: string;
+}
+
+export interface Message {
+  id: string;
+  chat_id: string;
+  user_id: string;
+  create_time: string;
+  text: string;
+  reply_msg_id: string;
 }
