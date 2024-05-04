0acaed148e16621edc903934e50cf8b81bde56d6
Kate <sochioneface@gmail.com>
2024-05-04 12:48:05 +0500

    fix navbar and add dialog window

--- front-angular/src/app/app.component.ts
+++ front-angular/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {AfterViewChecked, Component, DoCheck, OnChanges, OnInit} from '@angular/core';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
 import {HttpClient} from "@angular/common/http";
 import {take, tap} from "rxjs";
 
@@ -10,9 +10,14 @@ import {take, tap} from "rxjs";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements DoCheck{
   auth: boolean = false;
+  href: string = '';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
+  ngDoCheck() {
+    this.href = this.router.url;
+    console.log(this.href);
+  }
 }
