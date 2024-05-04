a48486772a07c6d162b29e8a056f34e22c249746
Kate <sochioneface@gmail.com>
2024-05-04 12:11:45 +0500

    add create chat button and create chat component

--- front-angular/src/app/api/api.ts
+++ front-angular/src/app/api/api.ts
@@ -3,7 +3,6 @@ import {HttpClient} from "@angular/common/http";
 import {RegisterBody, SignInBody} from "./api-interfaces";
 import {Injectable} from "@angular/core";
 import {apiRepo} from "./api.repo";
-import {parseJson} from "@angular/cli/src/utilities/json-file";
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +16,7 @@ export class apis {
     return this.http.post(url, list).pipe(take(1), tap((token) => this.repo.setToken(token)));
   }
 
-  signIn(list :SignInBody) {
+  signIn(list : SignInBody) {
     const url = `http://147.45.70.245:80/post/auth/user_by_auth`;
 
     return this.http.post(url, list).pipe(take(1), tap((data) => this.repo.setUser(data)));
